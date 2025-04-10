43e3e7c3e94bd545b563ce409e7bf9e3ebcb228e
Glenn Wahlbeck <glenn.wahlbeck@campusnykoping.se>
2025-04-10 14:37:25 +0200

    Added map and other stuff

--- script/coordinates.js
+++ script/coordinates.js
@@ -1,15 +1,50 @@
-var x = document.getElementById("demo");
+
 getLocation();
+let age;
+let lat = 58.673993;
+let long = 17.07306755;
+console.log(lat);
+var x = document.getElementById("demo");
+
+
 
 function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.watchPosition(showPosition);
-    } else {
-      x.innerHTML = "Geolocation is not supported by this browser.";
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
   }
-  
-  function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    " - Longitude: " + position.coords.longitude;
-  }
+}
+
+function showPosition(position) {
+
+  lat = position.coords.latitude;
+  long = position.coords.longitude;
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+  marker.setLatLng([lat, long]);
+  map.setView([lat, long]);
+
+}
+
+function checkAge(value) {
+  age = value
+  console.log(age)
+}
+
+// Initiera kartan med startkoordinater (ex. Stockholm) och zoomnivå
+var map = L.map('map').setView([lat, long], 19);
+
+// Lägg till OpenStreetMap som bakgrundskarta
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+var marker = L.marker([lat, long]).addTo(map);
+
+var circle = L.circle([58.673993, 17.07306755], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 5
+}).addTo(map);
---
+++ script/questions.js
@@ -0,0 +1,31 @@
+const questions =
+[
+    {
+        id: "egg1",
+        place: [58.67399, 17.07306],
+        barn: { fråga: "Vad blir 8 + 9?", svar: "17" },
+        vuxen: { fråga: "Vad blir 81 * 19?", svar: "1539" }
+      },
+    
+]
+
+
+/*{
+       
+    id: 1,
+    difficulty: "barn",
+    question: "Vad blir 8 + 9?",
+    answer: 17,
+    lat: 58.67399,
+    lon: 17.07306,
+    hint: "Nästa ledtråd är i sandlådan"
+},  
+{
+    id: 2,
+    difficulty: "vuxen",
+    question: "Vad blir 81 * 19?",
+    answer: 1539,
+    lat: 58.67399,
+    lon: 17.07306,
+    hint: "Nästa ledtråd är i sandlådan"
+}*/
