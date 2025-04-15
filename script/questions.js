12114d49698c3fafb11c0b725c94725ff5f658a8
Sara Westerberg <sara.westerberg8@campusnykoping.se>
2025-04-15 15:32:42 +0200

    added questionfunc

--- script/questions.js
+++ script/questions.js
@@ -1,31 +1,35 @@
-const questions =
-[
+let counter = 1;
+
+
+const questions = [
     {
-        id: "egg1",
-        place: [58.67399, 17.07306],
-        barn: { fråga: "Vad blir 8 + 9?", svar: "17" },
-        vuxen: { fråga: "Vad blir 81 * 19?", svar: "1539" }
-      },
-    
-]
+      id: 1,
+      place: [58.67399, 17.07306],
+      barn: { fråga: "Vad blir 8 + 9?", svar: "17" },
+      vuxen: { fråga: "Vad blir 81 * 19?", svar: "1539" }
+    },
+    {
+      id: 2,
+      place: [58.673993, 17.07306755],
+      barn: { fråga: "Vad heter Pippi Långstrumps apa?", svar: "Herr Nilsson" },
+      vuxen: { fråga: "Vad heter författaren till Pippi?", svar: "Astrid Lindgren" }
+    }
+  ];
 
 
-/*{
-       
-    id: 1,
-    difficulty: "barn",
-    question: "Vad blir 8 + 9?",
-    answer: 17,
-    lat: 58.67399,
-    lon: 17.07306,
-    hint: "Nästa ledtråd är i sandlådan"
-},  
-{
-    id: 2,
-    difficulty: "vuxen",
-    question: "Vad blir 81 * 19?",
-    answer: 1539,
-    lat: 58.67399,
-    lon: 17.07306,
-    hint: "Nästa ledtråd är i sandlådan"
-}*/
+  function getQuestion(counter, age) {
+    const questionobj = questions.find(q => q.id === counter);
+    if (!questionobj) return;
+  
+    const frågaText = questionobj[age].fråga;
+  
+    document.getElementById("question").innerHTML = frågaText;
+  
+    //const utterance = new SpeechSynthesisUtterance(frågaText);
+    //utterance.lang = 'sv-SE';
+    //speechSynthesis.speak(utterance);
+  }
+
+  
+// När du vill visa en fråga:
+getQuestion(1, "barn");
