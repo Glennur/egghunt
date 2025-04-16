4c63325dba9f0d3001c61bc176ee15d9a7a6f6ca
Glenn Wahlbeck <glenn.wahlbeck@campusnykoping.se>
2025-04-16 20:11:56 +0200

    changed question route

--- script/questions.js
+++ script/questions.js
@@ -21,7 +21,7 @@ const questions = [
     {
         bildURL: "paintegg.jpg",
         id: 1,
-        place: [58.67399, 17.07306],
+        place: [58.673540, 17.074206],
         barn: { fråga: "Vad brukar man måla till påsk?", svar: "Ägg" },
         vuxen: { fråga: "Vilken religiös högtid firas samtidigt som påsken?", svar: "Påsk" },
         feedback: "",
@@ -29,7 +29,7 @@ const questions = [
     {
         id: 2,
         bildURL: "easterbunny.jpg",
-        place: [58.674456, 17.072136],
+        place: [58.673352, 17.073273],
         barn: { fråga: "Vilket djur kommer med ägg till påsk enligt sagan?", svar: "Påskharen" },
         vuxen: { fråga: "I vilket land sägs påskharen ha sitt ursprung?", svar: "Tyskland" },
         feedback: ""
@@ -37,7 +37,7 @@ const questions = [
     {
         id: 3,
         bildURL: "langfredag.jpg",
-        place: [58.67430, 17.07380],
+        place: [58.673978, 17.073074],
         barn: { fråga: "Vad kallas fredagen före påskhelgen?", svar: "Långfredag" },
         vuxen: { fråga: "Vad minns man under långfredagen?", svar: "Jesus korsfästelse" },
         feedback: ""
@@ -45,7 +45,7 @@ const questions = [
     {
         id: 4,
         bildURL: "paskkarring.jpg",
-        place: [58.67450, 17.07410],
+        place: [58.674597, 17.073282],
         barn: { fråga: "Vad brukar barn klä ut sig till på påsken?", svar: "Påskkärring" },
         vuxen: { fråga: "Vad symboliserar fjädrarna i påskriset?", svar: "Livets återkomst" },
         feedback: ""
@@ -53,7 +53,7 @@ const questions = [
     {
         id: 5,
         bildURL: "chicken2.jpg",
-        place: [58.67470, 17.07440],
+        place: [58.674699, 17.075097],
         barn: { fråga: "Vilken färg är vanlig på påskägg?", svar: "Gul" },
         vuxen: { fråga: "Vilken färg symboliserar påsk i kyrkan?", svar: "Vit" },
         feedback: ""
@@ -61,7 +61,7 @@ const questions = [
     {
         id: 6,
         bildURL: "paskagg.jpg",
-        place: [58.67490, 17.07470],
+        place: [58.674170, 17.076355],
         barn: { fråga: "Vad brukar man lägga i påskägg?", svar: "Godis" },
         vuxen: { fråga: "Vad kallas traditionen att ge barn godis i påskägg?", svar: "Äggjakt" },
         feedback: ""
@@ -69,15 +69,15 @@ const questions = [
     {
         id: 7,
         bildURL: "easterlilly.jpg",
-        place: [58.67510, 17.07500],
+        place: [58.673582, 17.079377],
         barn: { fråga: "Vilken blomma är vanlig till påsk och är gul?", svar: "Påsklilja" },
         vuxen: { fråga: "Vilken blomma kallas ibland för narciss?", svar: "Påsklilja" },
         feedback: ""
     },
     {
-        id: 8,
+        id: 8, 
         bildURL: "chicken.jpg",
-        place: [58.67530, 17.07530],
+        place: [58.674751, 17.080026],
         barn: { fråga: "Vad heter kycklingens mamma?", svar: "Höna" },
         vuxen: { fråga: "Vad symboliserar ägget i kristen påsktradition?", svar: "Livets början" },
         feedback: ""
@@ -85,7 +85,7 @@ const questions = [
     {
         id: 9,
         bildURL: "jesus.jpg",
-        place: [58.67550, 17.07560],
+        place: [58.674572, 17.076346],
         barn: { fråga: "Vad heter söndagen då påsken börjar?", svar: "Påskdagen" },
         vuxen: { fråga: "Vad firar kristna på påskdagen?", svar: "Jesu uppståndelse" },
         feedback: ""
@@ -93,7 +93,7 @@ const questions = [
     {
         id: 10,
         bildURL: "blakulla.jpg",
-        place: [58.67570, 17.07590],
+        place: [58.673686, 17.074157],
         barn: { fråga: "Vad åker påskkärringarna till?", svar: "Blåkulla" },
         vuxen: { fråga: "Vad ansågs Blåkulla vara enligt folktron?", svar: "Djävulens festplats" },
         feedback: "Du är framme vid målet! Köp ägget själv!"
@@ -132,8 +132,8 @@ function checkAnswer() {
 
         counter++;
         setTimeout(() => {
-            //document.getElementById("question-container").style.display = "none";
-            document.getElementById("question-container").innerHTML = "";
+            document.getElementById("question-container").style.display = "none";
+            //document.getElementById("question-container").innerHTML = "";
           }, 1000);
           
         
